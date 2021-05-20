1771ba0c72139d5a5f24e047acfbf295756002bd
alastairmoody-madeye <akshata.lakshmi@gmail.com>
2021-05-20 16:45:47 +0530

    2 stage deletion

--- main.go
+++ main.go
@@ -8,30 +8,48 @@ import (
 )
 
 func main() {
-	l := list.NewLockList()
+	l := list.NewLockFreeList()
 	var wg sync.WaitGroup
 
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(i int, wg *sync.WaitGroup) {
-			err := l.Insert(int64(i))
-			if err != nil {
-				panic(err)
-			}
-			wg.Done()
-		}(i, &wg)
-	}
-	wg.Wait()
-	fmt.Println(l.Size)
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(i int, wg *sync.WaitGroup) {
-			err := l.Delete(int64(i))
-			if err != nil {
-				panic(err)
-			}
-			wg.Done()
-		}(i, &wg)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(1)
+	// 	go func(i int, wg *sync.WaitGroup) {
+	// 		err := l.Insert(int64(i))
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		wg.Done()
+	// 	}(i, &wg)
+	// }
+	// wg.Wait()
+	// fmt.Println(l.Size)
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(1)
+	// 	go func(i int, wg *sync.WaitGroup) {
+	// 		err := l.Delete(int64(i))
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		wg.Done()
+	// 	}(i, &wg)
+	// }
+	// wg.Wait()
+
+	l.Insert(10)
+	l.Insert(30)
+
+	wg.Add(2)
+	go func(i int, wg *sync.WaitGroup) {
+		err := l.Delete(int64(i))
+		fmt.Println(err)
+		list.PrintList(l)
+		wg.Done()
+	}(10, &wg)
+	go func(i int, wg *sync.WaitGroup) {
+		err := l.Insert(int64(i))
+		fmt.Println(err)
+		list.PrintList(l)
+		wg.Done()
+	}(20, &wg)
 	wg.Wait()
 }
